22b9c3aec6803871c8c8427a36829529dcbdd32f
Sailito D Porcadilla Jr <sporcadilla.jr.itso@tip.edu.ph>
2022-07-01 01:15:35 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/guidance-system/',
-  outputDir: 'dist',
+  outputDir: 'dist/',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
