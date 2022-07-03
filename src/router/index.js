1a28a28669e5d3f3d8e515c1d532855ea34f7d0c
Sailito D Porcadilla Jr <sporcadilla.jr.itso@tip.edu.ph>
2022-07-03 21:08:54 +0800

    update with display data from gql

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,12 @@ Vue.use(VueRouter)
 const client = process.env.VUE_APP_CLIENT
 const clientConstantRoutes = require('./' + client + '/constant_routes')
 const clientModuleRoutes = require('./' + client + '/modules')
-console.log('clientModuleRoutes', clientModuleRoutes)
 export const constantRoutes = [
   ...clientConstantRoutes.router
 ]
 export const asyncRoutes = [
   ...clientModuleRoutes.router
 ]
-
-console.log('asyncRoutes', asyncRoutes)
-
 const createRouter = () => new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
