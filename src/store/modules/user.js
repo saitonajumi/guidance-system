968663a6695d5dbe8f78430573ff92137ca6cb73
Sailito D Porcadilla Jr <sporcadilla.jr.itso@tip.edu.ph>
2022-07-07 04:13:34 +0800

    update

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -36,7 +36,6 @@ const actions = {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
         commit('SET_TOKEN', data.token)
-        setToken(data.token)
         resolve()
       }).catch(error => {
         reject(error)
